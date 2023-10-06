fe7096b71d9076949afd84c4690b6c1655f2ed44
Adelina <maheeshaadelina@gmail.com>
2023-10-06 12:07:08 +0530

    I said CREEPERS FIRST

--- src/main/java/io/github/adelinam17n/mixin/ExplosionMixin.java
+++ src/main/java/io/github/adelinam17n/mixin/ExplosionMixin.java
@@ -1,12 +1,19 @@
 package io.github.adelinam17n.mixin;
 
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.Explosion;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Explosion.class)
 public class ExplosionMixin {
+    @Shadow @Final private @Nullable Entity source;
+
     // Stops the loop from starting, more performant efficient as blocks are uselessly added to a list
     @ModifyVariable(
             method = "explode",
@@ -14,6 +21,6 @@ public class ExplosionMixin {
             ordinal = 1
     )
     private int doNotLetTheLoopRun(int value){
-        return 100;
+        return this.source instanceof Creeper ? 100 : value;
     }
 }
